917a5e8d189573f48bde7cbbdc96d3c3f649f3d1
GigaChadIsaFad <113807060+GigaChadIsaFad@users.noreply.github.com>
2022-09-19 11:00:56 -0700

    Update stormy.lua

--- stormy.lua
+++ stormy.lua
@@ -1,2 +1,16 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Stormy";
+Text = "Injecting";
+Icon = "";
+Duration = 5;
+})
+wait 0.5
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GigaChadIsaFad/private-stuff/main/Stormy', true))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/GigaChadIsaFad/private-stuff/main/Stormy%20loader%20main'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/GigaChadIsaFad/private-stuff/main/Stormy'))()
+wait 0.5
+game.StarterGui:SetCore("SendNotification", {
+Title = "Stormy";
+Text = "Injecting";
+Icon = "";
+Duration = 5;
+})
