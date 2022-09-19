cd15fa9fb97f98d535cbf8b75022ebcf717d8e7f
GigaChadIsaFad <113807060+GigaChadIsaFad@users.noreply.github.com>
2022-09-19 10:47:26 -0700

    Create stormy.lua

---
+++ stormy.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GigaChadIsaFad/private-stuff/main/Stormy%20loader%20main'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/GigaChadIsaFad/private-stuff/main/Stormy'))()
